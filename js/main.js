28bb94b944ba814eb84b8a2d341ec30123b66cdb
Anthony Urbina <aurbinava002@gmail.com>
2022-09-12 23:34:25 +0000

    Listen for 'submit' events on the journal entry form

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-var $photoURLInput = document.querySelector('form');
+var $form = document.querySelector('form');
 var $placeholderImg = document.querySelector('.placeholder-img');
+var $photoURLInput = document.querySelector('#photo-url');
 
 $photoURLInput.addEventListener('input', handleInput);
 
 function handleInput(event) {
-  $placeholderImg.setAttribute('src', event.target.value);
+  var enteredURL = event.target.value;
+  $placeholderImg.setAttribute('src', enteredURL);
+}
+
+$form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
 }

897d34422e5e1499ad1a34912ba8d6fac8902739
Anthony Urbina <aurbinava002@gmail.com>
2022-09-17 01:33:59 +0000

    Update the entry form's submit handler function to conditionally add a new entry DOM tree or replace the existing one.

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ var $ul = document.querySelector('ul');
 var $h2NewEntriesTitle = document.querySelector('#new-entries-title');
 var $h2EditEntryTitle = document.querySelector('#edit-entry-title');
 $photoURL.addEventListener('input', handleInput);
-
 function handleInput(event) {
   var enteredURL = event.target.value;
   $placeholderImg.setAttribute('src', enteredURL);
@@ -33,18 +32,15 @@ function handleSubmit(event) {
     var newEntryDomTree = createNewEntryLi(newEntry);
     $ul.prepend(newEntryDomTree);
   } else if ($h2EditEntryTitle.className === '') {
+    var $allLi = document.querySelectorAll('li');
     data.editing.entryTitle = $title.value;
     data.editing.entryPhotoURL = $photoURL.value;
     data.editing.entryNotes = $notes.value;
     for (var i = 0; i < data.entries.length; i++) {
       if (data.editing.entry === data.entries[i].entry) {
         data.entries[i] = data.editing;
-        newEntryDomTree = createNewEntryLi(newEntry);
-        // $ul.replaceChild(newEntryDomTree, )
-        //  YOU WERE FIGURING OUT HOW TO USE REPLACE CHILD
-        // GET NODE LIST DOCUMENT.QUEEARYSELECTORSLL('[data-entry-id[0]]') AND FIGURE OUT HOW TO GET THE EXACT LI
-        // console.log('data.editing', data.editing);
-        // console.log('data.entries[i]', data.entries[i]);
+        newEntryDomTree = createNewEntryLi(data.editing);
+        $ul.replaceChild(newEntryDomTree, $allLi[i]);
       }
     }
   }
@@ -175,30 +171,3 @@ function handleEditIconClick(event) {
     }
   }
 }
-
-// set attriute data-entry-id = newEntryId to each new li
-// listen for clicks onparent elemement of all rendered entries aka add event listener on the ul
-// if edit icon is clicked show the form
-// also, find the matching entry object in the data model and assign it to editing property if an edit item was clicked
-// basically, you'll run a for loop thats the length of entries so u can look through each one until you find the one with the entry id property
-// that matches the id property of the parent li of the button that was clicked (refer to dom delegation if u have to)
-// then, prepopulate the entry form with the clicked entries values from the object found in the data model
-// so by clicking on it, i can see which li its a part of, thus i can look inside of it, thus i can pull the data from there and insert it into my values for the edit box
-
-// update submit handler function to conditionally add a new entry object or update the existing one
-// under what conditions would you add a new entry object??
-// well to answet that you would have to log the +/- of  adding a new object vs updating the existing object
-// ok so lets assume im fine doing it the way that im doing now
-// we have a submit listener on the form that's really listening for the button to be clicked
-// once that happens a question is going to need to be answered
-// since new entry AND edit share the same page
-// is the data on the screen new data OR is it edited data?
-// an easy way to tell will be to check the title,
-// if the titles value is 'new entry', then you will act as if it is a new object
-// if the titles value is 'edit entry' then you will act as if it isn't and thus you will edit the already existing data
-// okay scrap that ^ do this v instead
-// if data.entries.editing = null  then do nothing, but if its not empty then do what?
-// well what do you want to happen?
-// if it's not null then i want to assign the values in the input boxes to the properties in the object that they originated from
-// when i submit i want to save the data to the corresponding object
-// how do i get the correspoding object
